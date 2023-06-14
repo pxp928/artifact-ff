86e9f5983654fe8c60586b8b23ba4bc4242a7c44
pxp928 <parth.psu@gmail.com>
2023-06-14 15:42:41 -0400

    add unit tests for noun separation
    Signed-off-by: pxp928 <parth.psu@gmail.com>

--- pkg/assembler/clients/helpers/parallel.go
+++ pkg/assembler/clients/helpers/parallel.go
@@ -166,7 +166,7 @@ func GetParallelAssembler(ctx context.Context, gqlclient graphql.Client) func([]
 			}
 
 			materials := p.GetMaterials(ctx)
-			logger.Infof("assembling Materials (Artifact): %v", len(materials))
+			logger.Infof("assembling Materials: %v", len(materials))
 			nouns.Go(func() error { return ingestMaterials(ctx, gqlclient, materials) })
 
 			cves := p.GetCVEs(ctx)
@@ -203,8 +203,6 @@ func GetParallelAssembler(ctx context.Context, gqlclient graphql.Client) func([]
 		if err := nouns.Wait(); err != nil {
 			return err
 		}
-
-		logger.Infof("completed ingesting all nouns")
 
 		verbs, ctx := errgroup.WithContext(ctx)
 
